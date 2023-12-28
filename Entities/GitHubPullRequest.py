9cf44700eac049686175d9de58ad5e5ab432d4b1
kinetic-coder <ojbullock@googlemail.com>
2023-12-28 19:29:31 +0000

    removed the database references

--- Entities/GitHubPullRequest.py
+++ Entities/GitHubPullRequest.py
@@ -1,6 +1,5 @@
 from Libraries.utilities import *
 from Repositories.GitHubPullRequestRepository import *
-import pyodbc
 
 class GitHubPullRequest:
     def __init__(self, id, title, body, user, created_at, updated_at, repo_name):
@@ -11,19 +10,3 @@ class GitHubPullRequest:
         self.updated_at = updated_at
         self.body = body
         self.repo_name = repo_name
-
-    def savePullRequests(pullRequests):
-        
-        connectionString = 'DRIVER={ODBC Driver 17 for SQL Server};' + \
-                                    'SERVER='+ get_environment_variable('DATABASE_SERVER') + ";" + \
-                                    'DATABASE='+ get_environment_variable('DATABASE_NAME') +";" + \
-                                    'UID='+ get_environment_variable('DATABASE_USERNAME') + ";" + \
-                                    'PWD='+ get_environment_variable('DATABASE_PASSWORD') + ";"
-        
-        print(connectionString)
-
-        dbConnection = pyodbc.connect(connectionString)
-
-        for pr in pullRequests:
-            db_repo = GitHubPullRequestRepository(dbConnection)
-            # print(f"{pr.id}, {pr.title}, {pr.user.login}, {pr.created_at}, {pr.updated_at}, {pr.body,}")T

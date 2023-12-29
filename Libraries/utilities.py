149e8e9bfc309c0943d461b1212c9e709dbf6ebe
kinetic-coder <ojbullock@googlemail.com>
2023-12-29 17:24:10 +0000

    Refactor code to use regular expressions and add JIRA reference to pull requests

--- Libraries/utilities.py
+++ Libraries/utilities.py
@@ -1,5 +1,5 @@
 import os
-from Libraries.utilities import *
+import re
 
 def get_secret(filename):
     with open(filename, 'r') as secret_file:
@@ -8,9 +8,9 @@ def get_secret(filename):
 
 def write_pull_requests_to_file(pull_requests, filename):
     with open(filename, 'w') as file:
-        file.write("id, repository, title, user, created_at, updated_at,\n")
+        file.write("id^ repository^ title^ user^ created_at^ updated_at^ jira_reference\n")
         for pr in pull_requests:
-            file.write(f"{pr.id}, {pr.repo_name}, {pr.title}, {pr.user.login}, {pr.created_at}, {pr.updated_at},\n")
+            file.write(f"{pr.id}^ {pr.repo_name}^ {pr.title}^ {pr.user.login}^ {pr.created_at}^ {pr.updated_at}^ {pr.jira_reference}^\n")
 
 def get_repos(filename):
     with open(filename, 'r') as repo_file:
@@ -19,3 +19,10 @@ def get_repos(filename):
 
 def get_environment_variable(variable_name):
     value = str(os.environ.get(variable_name))
+
+def parse_string_with_regex(input_string, regex_pattern):
+    match = re.search(regex_pattern, input_string)
+    if match:
+        return match.group()
+    else:
+        return None

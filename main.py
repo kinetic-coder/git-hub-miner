b75a8018d3dc0f145a97a6d17f99902d3ff027ac
kinetic-coder <ojbullock@googlemail.com>
2024-03-02 18:41:29 +0000

    Updated to get pull request reviews

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import os
-from UseCases.GitHubPullRequestHistory import GitHubUseCases
+from UseCases.GitHubUseCases import GitHubUseCases
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 def show_main_menu():
     print("1. Get git hub pull requests")
-    print("2. Get Jira information using jql")
+    print("2. Get GitHub pull request reviews")
     print("3. Exit")
     return input("Enter your choice: ")
 
 while True:
     clear_screen()
     choice = show_main_menu()
+
     if choice == "3":
         clear_screen()
         print("Thank you for using git-hub-miner - Bye!!")
         break
+    
     elif choice == "1":
         gh = GitHubUseCases()
         gh.get_github_pull_requests()
+        break
+    
     elif choice == "2":
-        print("Getting Jira information using jql")
+        gh = GitHubUseCases()
+        gh.get_github_pr_reviews()
+        break
+    
     else:
         print("Invalid choice")
